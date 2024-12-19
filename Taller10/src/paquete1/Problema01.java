7abc1430c9118e8cfbbef9016e6e2b4c1af178f7
MalejandroC <macarrion44@utpl.edu.ec>
2024-12-19 11:48:12 -0500

    cambios

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,22 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] notas = {{9,10,7,8},{10,5,7,9},{7,9.4,10,9}};
+        String [] estudiantes = {"Jerry Ponce","Gabriela Lewis","David Bell"};
+        double [] promedios = new double [3];
+        double suma;
+        int col;
+        for (int fila = 0; fila < notas.length; fila++) {
+            suma = 0;
+             for (col = 0; col < notas[fila].length; col++) {
+                 suma = suma + notas [fila][col];
+             }
+             promedios[fila] = suma / col;           
+        }
+         for (int fila = 0; fila < notas.length; fila++) {
+                 System.out.printf("Estudiante: %s tiene un promedio de %.2f\n", estudiantes[fila],promedios[fila]);
+             }
+        
     }
     
 }
